c472d64bd182cc78f913402334e0b776c203855e
izanms0.0 <88353965+izanms0-0@users.noreply.github.com>
2021-10-05 12:14:04 +0200

    Update op1o.py

--- historia2/op1o.py
+++ historia2/op1o.py
@@ -27,5 +27,25 @@ os.system('cls')
 print("llegas a las puertas de tu instituto y estan cerradas")
 time.sleep(3)
 os.system('cls')
+#Menu 
+def menu():
+    print("1-) Esperar")
+    print("")
+    print("2-) ir a un bar")
+    print("\n >>")
+    
 while True:
     input1 = input("Que quieres hacer?\n\n")
+    menu()
+    if input1==("1"):{
+        os.system('cls')
+        os.system('python esperar.py')
+    }
+    
+    elif input1==("2"):{
+        break
+    }
+        
+    else:
+        input('Esa opcion no esta en la lista...\nPulsa enter para continuar')
+break
