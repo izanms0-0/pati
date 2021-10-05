fc6a8df43c9c186ecb38b0e344c059cf15e1aefc
izanms0.0 <88353965+izanms0-0@users.noreply.github.com>
2021-10-05 13:39:36 +0200

    Update op1o.py

--- historia2/op1o.py
+++ historia2/op1o.py
@@ -6,13 +6,17 @@ os.system('cls')
 
 #VARIABLES GLOBALES
 
+timepo3 = time.sleep(3)
+tiempo2 = time.sleep(2)
+tiempo1 = time.sleep(1)
+cls = os.system('cls')
 rojo = Fore.RED
 verde = Fore.GREEN
 rosa = Fore.MAGENTA
 resetear = Style.RESET_ALL
 
 #Codigo
-print("Cojes tu mochila y sales de tu casa...")
+print("X: Cojes tu mochila y sales de tu casa...")
 time.sleep(3)
 os.system('cls')
 print("7:00")
@@ -24,7 +28,7 @@ os.system('cls')
 print("7:00")
 time.sleep("7:00")
 os.system('cls')
-print("llegas a las puertas de tu instituto y estan cerradas")
+print("X: llegas a las puertas de tu instituto y estan cerradas")
 time.sleep(3)
 os.system('cls')
 #Menu 
@@ -43,9 +47,19 @@ while True:
     }
     
     elif input1==("2"):{
+        os.system('cls')
+        time.sleep(1.3)
         break
     }
         
     else:
         input('Esa opcion no esta en la lista...\nPulsa enter para continuar')
 break
+##############################################################################################################################################
+
+print("X: caminas hasta un bar a la vuelta de la esquina...")
+time.sleep(2)
+cls
+print("7:15")
+tiempo2
+cls
