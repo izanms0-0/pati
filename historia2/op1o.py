2092f8aeb63842a72f99bea21e2e98b31489bb82
izanms0.0 <88353965+izanms0-0@users.noreply.github.com>
2021-10-05 10:08:38 +0200

    Create op1o.py

---
+++ historia2/op1o.py
@@ -0,0 +1,31 @@
+import os 
+import time
+import colorama
+
+os.system('cls')
+
+#VARIABLES GLOBALES
+
+rojo = Fore.RED
+verde = Fore.GREEN
+rosa = Fore.MAGENTA
+resetear = Style.RESET_ALL
+
+#Codigo
+print("Cojes tu mochila y sales de tu casa...")
+time.sleep(3)
+os.system('cls')
+print("7:00")
+time.sleep(0.7)
+os.system('cls')
+print("7:00")
+time.sleep(0.7)
+os.system('cls')
+print("7:00")
+time.sleep("7:00")
+os.system('cls')
+print("llegas a las puertas de tu instituto y estan cerradas")
+time.sleep(3)
+os.system('cls')
+while True:
+    input1 = input("Que quieres hacer?\n\n")
