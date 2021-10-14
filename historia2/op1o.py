d55f36a9f2e68c59b982c6c63dae8ff93ee517e6
izanms0.0 <88353965+izanms0-0@users.noreply.github.com>
2021-10-14 21:58:57 +0200

    Update op1o.py

--- historia2/op1o.py
+++ historia2/op1o.py
@@ -4,6 +4,36 @@ from colorama import *
 
 os.system('cls')
 
+#cajetilla para def
+def menu1():
+    print("Que quieres tomar?")
+    print("")
+    print("1-)" + Fore.RED + " Agua" + Style.RESET_ALL)
+    print("2-) Zumo de naranja")
+    print("3-) Granizado")
+    print("4-) Nada")
+    input2 = input(">>")
+    
+    if input2 == ("1"):
+        time.sleep(0.5)
+        os.system('python agua.py')
+    
+    elif input2 == ("2"):
+        time.sleep(0.5)
+        os.system('python zumo.py')
+        
+    elif input2 == ("3"):
+        time.sleep(0.5)
+        os.system("python granizado.py")
+        
+    elif input2 == ("4"):
+        time.sleep(0.5)
+        
+    
+    else:
+        input(Fore.LIGHTMAGENTA_EX + "Esa opcion no esta en la lista...\nPulsa Enter para continuar" + Style.RESET_ALL)
+        os.system('cls')
+
 #VARIABLES GLOBALES
 
 rojo = Fore.RED
@@ -28,37 +58,34 @@ time.sleep(1)
 print(Style.BRIGHT + "7:00a.m ")
 time.sleep(1)
 os.system('cls')
+time.sleep(0.4)
 print("X: llegas a las puertas de tu instituto y estan cerradas")
 time.sleep(2.5)
 os.system('cls')
 #Menu 
-def menu():
-    """
-    Funcion de reseteo
-    """
-    while True:
-        input1 = input("Que quieres hacer?\n" + "\n1-) Esperar" + "\n2-) ir a un bar" + "\n\n>>")
-
-        if input1==("1"):
-            os.system('cls')
-            os.system('python esperar.py')
+while True:
+    input1 = input("Que quieres hacer?\n" + "\n1-) Esperar" + "\n2-) ir a un bar" + "\n\n>>")
+    
+    if input1==("1"):
+        os.system('cls')
+        os.system('python esperar.py')
 			
-        elif input1==("2"):
-            os.system('cls')
-            time.sleep(1.3)
+    elif input1==("2"):
+        os.system('cls')
+        time.sleep(1.3)
+        break
         
-        else:
-            input(Fore.LIGHTMAGENTA_EX + 'Esa opcion no esta en la lista...\nPulsa enter para continuar' + Style.RESET_ALL)  
-        break    
+    else:
+        input(Fore.LIGHTMAGENTA_EX + 'Esa opcion no esta en la lista...\nPulsa enter para continuar' + Style.RESET_ALL)  
+        os.system('cls')
+            
+            
 ##############################################################################################################################################
 
-menu()
-
+    
+##################################################################################################################
 print("X: caminas hasta un bar a la vuelta de la esquina...")
 time.sleep(2)
-os.system('cls')
-print("7:15")
-time.sleep()
 os.system('cls')
 print("X: Entras al bar y te sientas")
 time.sleep(3)
@@ -66,18 +93,7 @@ os.system('cls')
 print("X: Pocos segundos despues viene un mesero y te pregunta...")
 time.sleep(3)
 os.system('cls')
-def  menu1():
-    input("X: Quieres pedir algo?")
-    print("")
-    print("1-) si")
-	print("")
-	print("2-) no")
-	print("")
-	print(">>")
-	
+
 while True:
-	menu()
-	if input == ("1"):{
-		menu2()
-		input("X: que quieres tomar")
-	}
+    menu1()
+    
